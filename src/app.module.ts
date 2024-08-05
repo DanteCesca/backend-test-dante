3b6c4d783a8987331698676c41a203787affc00d
DanteCesca <dantecesca597@gmail.com>
2024-08-05 19:03:43 -0300

    feat(auth): Se crearon los modulos, controladores y servicions de autenticacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TareaModule } from './tarea/tarea.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TareaModule],
+  imports: [TareaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
