6093d79c6cce0fff8b8b6c127270626cfe898893
DanteCesca <dantecesca597@gmail.com>
2024-08-05 16:37:39 -0300

    feat(tarea.module.entity.ts): Se creo la entidad Tarea y se configuro el modulo para inluirla

--- src/tarea/tarea.module.ts
+++ src/tarea/tarea.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TareaService } from './tarea.service';
 import { TareaController } from './tarea.controller';
+import { Tarea } from './tarea.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Tarea])],
   providers: [TareaService],
   controllers: [TareaController]
 })
