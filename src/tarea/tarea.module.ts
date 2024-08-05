8b7cc9377148e4184dbbedd1b91f7542fc91ad02
DanteCesca <dantecesca597@gmail.com>
2024-08-05 16:06:52 -0300

    feat(tarea): Se genero un modulo, servicio y controlador para tareas

---
+++ src/tarea/tarea.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TareaService } from './tarea.service';
+import { TareaController } from './tarea.controller';
+
+@Module({
+  providers: [TareaService],
+  controllers: [TareaController]
+})
+export class TareaModule {}
